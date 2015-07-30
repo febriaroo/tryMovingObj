9bb83c5be8ca59b31341c4314c59c35964ce2278
febriaroo <febriaroosita@gmail.com>
2015-07-30 14:44:49 -0400

    Initial Commit

---
+++ tryingMovingObject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tryingMovingObject
+//
+//  Created by Febria Roosita Dwi on 7/30/15.
+//  Copyright (c) 2015 Febria Roosita Dwi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
