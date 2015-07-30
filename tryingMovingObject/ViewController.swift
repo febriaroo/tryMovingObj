411637a7dc6b71039d48e130443646b4de92d629
febriaroo <febriaroosita@gmail.com>
2015-07-30 15:37:04 -0400

    tryToMoveObj
    This how to move one object from one postition to other position base
    on user touch position

--- tryingMovingObject/ViewController.swift
+++ tryingMovingObject/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var location = CGPoint(x: 0, y: 0)
+    
+    
+    @IBOutlet weak var bawah: UIView!
+    
+    // When user starting to touch the phone
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if let touch = touches.first as? UITouch {
+            location = touch.locationInView(self.view)
+            bawah.center = location
+        }
+        super.touchesBegan(touches , withEvent:event)
+        
+    }
+    // When user starting to move when they are still touching the screen
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if let touch = touches.first as? UITouch {
+             location = touch.locationInView(self.view)
+            bawah.center = location
+        }
+        super.touchesBegan(touches , withEvent:event)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
