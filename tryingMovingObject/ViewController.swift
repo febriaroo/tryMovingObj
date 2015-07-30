9aa7717b5b8f7cc7b7b93d14d3ead55271050591
febriaroo <febriaroosita@gmail.com>
2015-07-30 17:05:33 -0400

    Adding the swipe gesture to move the block

--- tryingMovingObject/ViewController.swift
+++ tryingMovingObject/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         super.touchesBegan(touches , withEvent:event)
         
     }
-    // When user starting to move when they are still touching the screen
+     //When user starting to move when they are still touching the screen
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
         if let touch = touches.first as? UITouch {
              location = touch.locationInView(self.view)
@@ -35,7 +35,14 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        
+        var downSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        leftSwipe.direction = .Up
+        downSwipe.direction = .Down
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(downSwipe)
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,6 +50,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func handleSwipes(sender:UISwipeGestureRecognizer) {
+        if (sender.direction == .Up) {
+            println("Swipe Up")
+            UIView.animateWithDuration(0.5, delay: 0, options: .CurveEaseOut, animations: {
+                var basketTopFrame = self.bawah.frame
+                basketTopFrame.origin.y -= basketTopFrame.size.height
+                
+               // var basketBottomFrame = self.basketBottom.frame
+                //basketBottomFrame.origin.y += basketBottomFrame.size.height
+                
+                self.bawah.frame = basketTopFrame
+                //self.basketBottom.frame = basketBottomFrame
+                }, completion: { finished in
+                    println("Basket doors opened!")
+            })
+        } else if (sender.direction == .Down){
+            println("Swipe Down")
+            UIView.animateWithDuration(0.5, delay: 0, options: .CurveEaseOut, animations: {
+//                var basketTopFrame = self.bawah.frame
+//                basketTopFrame.origin.y -= basketTopFrame.size.height
+                
+                 var basketBottomFrame = self.bawah.frame
+                basketBottomFrame.origin.y += basketBottomFrame.size.height
+                
+                //self.bawah.frame = basketTopFrame
+                self.bawah.frame = basketBottomFrame
+                }, completion: { finished in
+                    println("Basket doors opened!")
+            })
+        }
+    }
 
 }
 
